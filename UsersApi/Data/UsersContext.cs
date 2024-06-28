44170641b49e8efc5dfa6e7780617d326b73e8b5
Tiago Teixeira <E1154@itsector.pt>
2024-06-28 16:08:34 +0100

    integration with postgreSQL

---
+++ UsersApi/Data/UsersContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersApi.Models;
+
+namespace UsersApi.Data
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
